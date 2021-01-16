f69fb3ec716b59d7d9d66079e565ac8c106e4fe8
Lucas Lofaro <lucaslofaro@gmail.com>
2021-01-15 19:35:48 -0800

    update package info

--- setup.py
+++ setup.py
@@ -1,26 +1,38 @@
 from setuptools import setup, find_packages
 
-tests_require = [
+requirements = (
+    'boto3',
+    'colorama',
+    'pyYaml',
+)
+
+tests_requirements = (
     'flake8',
     'coverage',
     'moto==1.3.14',
     'pyfakefs',
     'pytest',
     'pytest-cov'
-]
+)
+
+
+def get_long_description(filename='README.md'):
+    with open(filename) as file:
+        readme = file.read()
+    return readme
+
 
 setup(
     name='cabinets',
     version='0.1.0',
-    description="Place to store many things.",
+    description="A consistent approach to file operations.",
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
     author="Lucas Lofaro, Sam Hollenbach",
     author_email="lucasmlofaro@gmail.com, samhollenbach@gmail.com",
-    packages=find_packages(exclude=['test', 'test_*', 'fixtures']),
-    install_requires=[
-        'boto3',
-        'colorama',
-        'pyYaml',
-    ],
-    tests_require=tests_require,
-    extras_require={'test': tests_require},
+    url='https://github.com/lucasmlofaro/cabinets',
+    packages=find_packages(exclude=['test']),
+    install_requires=requirements,
+    tests_require=tests_requirements,
+    extras_require={'test': tests_requirements},
 )
