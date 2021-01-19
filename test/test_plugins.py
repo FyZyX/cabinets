13b352b554b6b1f1c319b059b866be07630db32a
Sam Hollenbach <samhollenbach@gmail.com>
2021-01-18 23:21:49 -0800

    Clear all extensions and protocols before each test

--- test/test_plugins.py
+++ test/test_plugins.py
@@ -1,32 +1,36 @@
 import os
 from unittest import TestCase
 
-from cabinets import plugins, SUPPORTED_PROTOCOLS, SUPPORTED_EXTENSIONS
+import cabinets
 
 
 class TestPlugins(TestCase):
 
-    def test_load_plugins_from_built_in(self):
-        plugins.discover_all(custom_plugin_path=None)
-        self.assertNotIn('mock', SUPPORTED_PROTOCOLS)
-        self.assertNotIn('mock', SUPPORTED_EXTENSIONS)
+    def setUp(self) -> None:
+        cabinets.SUPPORTED_PROTOCOLS.clear()
+        cabinets.SUPPORTED_EXTENSIONS.clear()
 
-        self.assertIn('file', SUPPORTED_PROTOCOLS)
-        self.assertIn('s3', SUPPORTED_PROTOCOLS)
-        self.assertIn('yml', SUPPORTED_EXTENSIONS)
-        self.assertIn('yaml', SUPPORTED_EXTENSIONS)
-        self.assertIn('json', SUPPORTED_EXTENSIONS)
-        self.assertIn('pickle', SUPPORTED_EXTENSIONS)
+    def test_load_plugins_from_built_in(self):
+        cabinets.plugins.discover_all(custom_plugin_path=None)
+        self.assertNotIn('mock', cabinets.SUPPORTED_PROTOCOLS)
+        self.assertNotIn('mock', cabinets.SUPPORTED_EXTENSIONS)
+
+        self.assertIn('file', cabinets.SUPPORTED_PROTOCOLS)
+        self.assertIn('s3', cabinets.SUPPORTED_PROTOCOLS)
+        self.assertIn('yml', cabinets.SUPPORTED_EXTENSIONS)
+        self.assertIn('yaml', cabinets.SUPPORTED_EXTENSIONS)
+        self.assertIn('json', cabinets.SUPPORTED_EXTENSIONS)
+        self.assertIn('pickle', cabinets.SUPPORTED_EXTENSIONS)
 
     def test_load_plugins_from_custom_path(self):
         plugin_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'plugins')
-        plugins.discover_all(custom_plugin_path=plugin_path)
-        self.assertIn('mock', SUPPORTED_PROTOCOLS)
-        self.assertIn('mock', SUPPORTED_EXTENSIONS)
+        cabinets.plugins.discover_all(custom_plugin_path=plugin_path)
+        self.assertIn('mock', cabinets.SUPPORTED_PROTOCOLS)
+        self.assertIn('mock', cabinets.SUPPORTED_EXTENSIONS)
 
-        self.assertIn('file', SUPPORTED_PROTOCOLS)
-        self.assertIn('s3', SUPPORTED_PROTOCOLS)
-        self.assertIn('yml', SUPPORTED_EXTENSIONS)
-        self.assertIn('yaml', SUPPORTED_EXTENSIONS)
-        self.assertIn('json', SUPPORTED_EXTENSIONS)
-        self.assertIn('pickle', SUPPORTED_EXTENSIONS)
+        self.assertIn('file', cabinets.SUPPORTED_PROTOCOLS)
+        self.assertIn('s3', cabinets.SUPPORTED_PROTOCOLS)
+        self.assertIn('yml', cabinets.SUPPORTED_EXTENSIONS)
+        self.assertIn('yaml', cabinets.SUPPORTED_EXTENSIONS)
+        self.assertIn('json', cabinets.SUPPORTED_EXTENSIONS)
+        self.assertIn('pickle', cabinets.SUPPORTED_EXTENSIONS)
