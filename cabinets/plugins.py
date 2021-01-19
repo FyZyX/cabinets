65d07cbce0501d780e3cbada89a72775f26f4c7b
Lucas Lofaro <lucaslofaro@gmail.com>
2021-01-18 19:30:06 -0800

    import custom plugins from package directly

--- cabinets/plugins.py
+++ cabinets/plugins.py
@@ -9,18 +9,18 @@ import cabinets.parser
 from cabinets.logger import info
 
 
-def discover(path, prefix):
-    plugins = pkgutil.iter_modules(path, prefix + '.')
+def discover(path, prefix=''):
+    plugins = pkgutil.iter_modules(path, prefix)
     for _, name, _ in plugins:
         importlib.import_module(name)
         info(f"Loaded plugin '{name}'")
 
 
 def discover_all(custom_plugin_path=None):
-    discover(cabinets.cabinet.__path__, prefix=cabinets.cabinet.__name__)
-    discover(cabinets.parser.__path__, prefix=cabinets.parser.__name__)
+    discover(cabinets.cabinet.__path__, prefix=cabinets.cabinet.__name__ + '.')
+    discover(cabinets.parser.__path__, prefix=cabinets.parser.__name__ + '.')
     if custom_plugin_path:
-        sys.path.insert(1, custom_plugin_path)
         for pkg in ['cabinet', 'parser']:
             path = os.path.join(custom_plugin_path, pkg)
-            discover((path,), prefix=pkg)
+            sys.path.insert(1, path)
+            discover((path,))
