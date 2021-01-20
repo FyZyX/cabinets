5d57e424aa1dadfcc4f158553499221cbae3425e
Sam Hollenbach <samhollenbach@gmail.com>
2021-01-20 11:01:48 -0800

    Version 0.3.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md') as file:
 
 setup(
     name='cabinets',
-    version='0.2.1',
+    version='0.3.0',
     description="A consistent approach to file operations, anywhere.",
     long_description=readme,
     long_description_content_type='text/markdown',
