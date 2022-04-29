ddbe3b153d69594b600e482e2c85f160ab0ec1ea
Sam Hollenbach <samhollenbach@gmail.com>
2022-04-28 23:07:51 -0700

    remove bad type hint

--- cabinets/cabinet/s3_cabinet.py
+++ cabinets/cabinet/s3_cabinet.py
@@ -68,7 +68,7 @@ class S3Cabinet(Cabinet):
             return False
 
     @classmethod
-    def list(cls, directory: str, delimiter: str = '/', **kwargs) -> List[str]:
+    def list(cls, directory, delimiter: str = '/', **kwargs) -> List[str]:
         cls._ensure_client_exists()
 
         bucket, *dir = directory.split(delimiter)
